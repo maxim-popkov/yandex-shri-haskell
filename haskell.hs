a93af1b70ef7e12bdd1fe3eba7e0d1e04bc0f0b7
maxim-popkov <maxim.popkov@outlook.com>
2013-09-25 04:24:10 +0400

    add comments

--- haskell.hs
+++ haskell.hs
@@ -17,25 +17,33 @@ getInt x xs = ord (xs !! x) - ord('0')
 ----------------------------------------
 
 -------------minimal price-------------
+--incidenc matrix
+matrix :: [[Int]]
 matrix = [[0,1,10,0],[1,0,4,1],[10,4,0,1],[0,1,1,0]]
 
 path :: [Int]
 path = [0,1,3,2]
 
+--compute price for path
 pathPrice path mtx = sum[mtx !! (path !! x) !! (path !! (x+1) ) | x <- [0.. (length(path)-2)]]
 
+--for graph vertex v1, return all neighbor vertex that not in blackList
 getNextVert x mtx blackList = [ y | y <-[0.. (length mtx)-1], mtx !! x !! y > 0, not (elem y blackList) ]
 
+--Search paths from v1 to v2 on graph
+--SearchPath Dont Work
 searchPath v1 v2 mtx blackList = (if v1 == v2
 									then blackList
-									else searchPath (getNextVert v1 mtx (v1:blackList)) v2 mtx (v1:blackList) )   
+									else (getNextVert v1 mtx (v1:blackList)) )
+									--else searchPath (getNextVert v1 mtx (v1:blackList)) v2 mtx (v1:blackList) )   
 ---------------------------------------
-searchStep vs v2 mtx blackList = searchPath head vs
 
 
 -----------factorian-------------------
+--compute factorial x
 fac x = product [1..x]
 
+--convert int string to int array
 --xs - int string;
 --return - int array
 --example: getIntArray "1234" => [1,2,3,4]  
@@ -44,9 +52,12 @@ getIntArray xs = ( if length xs == 1
 					else ord(xs !! 0) - ord('0') : getIntArray (tail xs) )
 --[x| x <- [1..50000], sum[fac y | y <- intArray(x) ] == x ]
 
-searchFactorion nums = [x| x <- nums, sum[fac y | y <- getIntArray(show x) ] == x ]  
+--SearchFactorian
+searchFactorion nums = [ x | x <- nums, sum[fac y | y <- getIntArray(show x) ] == x ]  
 --------------------------------------
 
 main = do
+	--test string convertion
 	print $ getIntArray "1223"
-	print $ searchFactorion [1..10000]
+	--search factorian
+	print $ searchFactorion [1..50000]
